13f3437517fcac2c69e197e5b284759f5228effc
Shreyas Lalge <shreyaslalge15@gmail.com>
2023-10-09 22:55:02 +0530

    "commented projects image code temporarily"

--- src/components/ProjectDetails/index.jsx
+++ src/components/ProjectDetails/index.jsx
@@ -66,13 +66,13 @@ const Desc = styled.div`
     }
 `;
 
-const Image = styled.img`
-    width: 100%;
-    object-fit: cover;
-    border-radius: 12px;
-    margin-top: 30px;
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.3);
-`;
+// const Image = styled.img`
+//     width: 100%;
+//     object-fit: cover;
+//     border-radius: 12px;
+//     margin-top: 30px;
+//     box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.3);
+// `;
 
 const Label = styled.div`
     font-size: 20px;
@@ -198,7 +198,7 @@ const index = ({ openModal, setOpenModal }) => {
                         }}
                         onClick={() => setOpenModal({ state: false, project: null })}
                     />
-                    <Image src={project?.image} />
+                    {/* <Image src={project?.image} alt ="image not available"/> */}
                     <Title>{project?.title}</Title>
                     <Date>{project.date}</Date>
                     <Tags>
